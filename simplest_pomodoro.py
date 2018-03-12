b22800ec77cf659f01d4338f44dbb4e385a06672
Megg Gawat <megawatt@live.com>
2018-03-12 09:27:41 -0400

    Fixed time to be 25 minutes instead of 15 seconds

--- simplest_pomodoro.py
+++ simplest_pomodoro.py
@@ -3,8 +3,9 @@ import random
 import time
 
 # Le Internet Medley, Nyan Cat, Caramelldansen, All Your Base Are Belong To Us, Tunak Tunak Tun, All Star
-alarm_video_urls = ["https://www.youtube.com/watch?v=mghhLqu31cQ", "https://www.youtube.com/watch?v=QH2-TGUlwu4", "https://www.youtube.com/watch?v=EBOWWbCf-KU",
-                    "https://www.youtube.com/watch?v=8fvTxv46ano", "https://www.youtube.com/watch?v=vTIIMJ9tUc8", "https://www.youtube.com/watch?v=L_jWHffIx5E",]
+alarm_video_urls = ["https://www.youtube.com/watch?v=mghhLqu31cQ", "https://www.youtube.com/watch?v=QH2-TGUlwu4",
+                    "https://www.youtube.com/watch?v=EBOWWbCf-KU", "https://www.youtube.com/watch?v=8fvTxv46ano",
+                    "https://www.youtube.com/watch?v=vTIIMJ9tUc8", "https://www.youtube.com/watch?v=L_jWHffIx5E",]
 
 def tick_time(seconds):
     for s in range(1, seconds+1):
@@ -12,7 +13,7 @@ def tick_time(seconds):
         print(s)
 
 def main():
-    tick_time(15)
+    tick_time(1500)
     chosen_video = random.choice(alarm_video_urls)
     webbrowser.open(chosen_video)
 
